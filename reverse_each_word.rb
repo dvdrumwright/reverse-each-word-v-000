a4bd65e8a05e53fc2ffee7b832ade266552849e6
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-03-21 16:55:16 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(sentence = "Hello there, and how are you?")
   sentence.reverse!
-  array = sentence.split(" ")
-  array.reverse!
-  puts array.join(" ")
+  entence.split(" ")
+  
 end
